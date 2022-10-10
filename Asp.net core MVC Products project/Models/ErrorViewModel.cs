8f3307883bbe3035646d791afe3e57418a479a1f
naseem <naseemhijazi.786786@gmail.com>
2022-10-10 21:38:57 +0500

    Add project files.

---
+++ Asp.net core MVC Products project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.net_core_MVC_Products_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
